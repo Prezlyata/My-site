0aa6c4239aa9f2806ba41002dff522a6a013478f
Taras Prezlyata <Taras Prezlyata>
2017-05-21 20:29:42 +0300

    my first commit

--- website/wsgi.py
+++ website/wsgi.py
@@ -15,3 +15,12 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
 
 application = get_wsgi_application()
 
+
+
+
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
